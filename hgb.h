e81e8948c544a3552eecf3897ed42b8890e1ff32
hotgluebanjo <66244111+hotgluebanjo@users.noreply.github.com>
2024-01-27 10:02:58 +0000

    Fix possible conflicts and expand

--- hgb.h
+++ hgb.h
@@ -1,13 +1,18 @@
-// HGB DCTL header 0.1.0
-// ---------------------
+// HGB DCTL library 0.1.0
+// ----------------------
 // Attempting to be compatible with all DCTL targets.
 // - https://github.com/hotgluebanjo
 //
 // References:
 //
+// - https://registry.khronos.org/OpenCL/specs/3.0-unified/pdf/OpenCL_C.pdf
 // - https://developer.apple.com/metal/Metal-Shading-Language-Specification.pdf
 // - https://docs.nvidia.com/cuda/pdf/CUDA_C_Programming_Guide.pdf
 //
+// TODO: Are the default floatN types operator-overloaded for OpenCL?
+// Since unions can't have anonymous structs it seems like a good idea to
+// simply transmute floatN instances for indexing rather than make a custom
+// VecN type.
 
 // Some, uh, assumptions. stdint is not included.
 typedef unsigned char   u8;
@@ -15,13 +20,13 @@ typedef unsigned short u16;
 typedef unsigned int   u32;
 typedef unsigned long  u64;
 
-typedef signed char   s8;
-typedef signed short s16;
-typedef signed int   s32;
-typedef signed long  s64;
+typedef signed char   i8;
+typedef signed short i16;
+typedef signed int   i32;
+typedef signed long  i64;
 
-typedef size_t    uint;
-typedef ptrdiff_t sint;
+typedef size_t    usize;
+typedef ptrdiff_t isize;
 
 typedef float f32;
 
@@ -41,7 +46,7 @@ typedef float Mat4[4][4];
 
 #ifdef HGB_LANG
 #define loop while (true)
-#define for_range(i, min, max) for (uint i = min; i < max; i += 1)
+#define for_range(i, min, max) for (usize i = min; i < max; i += 1)
 #define cast(T) (T)
 #define transmute(x, T) (*((T)(&x)))
 #endif
@@ -53,6 +58,9 @@ typedef float Mat4[4][4];
 
 #define HGB_SQRT_TWO 1.41421356237309504880168872420969808f
 #define HGB_SQRT_THREE 1.73205080756887729352744634150587236f
+
+#define HGB_ERROR make_float3(1.0f, 0.0f, 0.0f)
+#define HGB_WARNING make_float3(1.0f, 1.0f, 0.0f)
 
 // Computes x raised to the power of y
 __DEVICE__ inline f32 hgb_pow(f32 x, f32 y) { return _powf(x, y); }
@@ -126,8 +134,7 @@ __DEVICE__ inline f32 hgb_acos(f32 x) { return _acosf(x); }
 // Computes the principle value of the arc sine of x
 __DEVICE__ inline f32 hgb_asin(f32 x) { return _asinf(x); }
 
-// Computes the principal value of the arc tangent of y/x, using the signs of
-// both arguments to determine the quadrant of the return value
+// Computes the principal value of the arc tangent of y/x, using the signs of both arguments to determine the quadrant of the return value
 __DEVICE__ inline f32 hgb_atan2(f32 y, f32 x) { return _atan2f(y, x); }
 
 // Computes the principle value of the inverse hyperbolic cosine of x
@@ -148,8 +155,7 @@ __DEVICE__ inline f32 hgb_sinh(f32 x) { return _sinhf(x); }
 // Computes the hyperbolic tangent of x
 __DEVICE__ inline f32 hgb_tanh(f32 x) { return _tanhf(x); }
 
-// Returns the positive difference between x and y:  x - y if x > y, +0 if
-// x is less than or equal to y
+// Returns the positive difference between x and y:  x - y if x > y, +0 if x is less than or equal to y
 __DEVICE__ inline f32 hgb_fdim(f32 x, f32 y) { return _fdimf(x, y); }
 
 // Computes (x * y) + z as a single operation
@@ -176,13 +182,11 @@ __DEVICE__ inline bool hgb_signbit(f32 x) { return bool(signbit(x)); }
 // Returns x with its sign changed to y's
 __DEVICE__ inline f32 hgb_copysign(f32 x, f32 y) { return _copysignf(x, y); }
 
-// Extract mantissa and exponent from x. The mantissa m returned is a f32 with
-// magnitude in the interval [1/2, 1) or 0, and exp is updated with integer exponent
-// value, whereas x = m * 2^exp
-__DEVICE__ inline f32 hgb_frexp(f32 x, s32 exp) { return _frexp(x, exp); }
+// Extract mantissa and exponent from x. The mantissa m returned is a f32 with magnitude in the interval [1/2, 1) or 0, and exp is updated with integer exponent value, whereas x = m * 2^exp
+__DEVICE__ inline f32 hgb_frexp(f32 x, i32 exp) { return _frexp(x, exp); }
 
 // Returns (x * 2^exp)
-__DEVICE__ inline f32 hgb_ldexp(f32 x, s32 exp) { return _ldexp(x, exp); }
+__DEVICE__ inline f32 hgb_ldexp(f32 x, i32 exp) { return _ldexp(x, exp); }
 
 #define hgb_square(x) ((x) * (x))
 #define hgb_cube(x) ((x) * (x) * (x))
@@ -204,64 +208,93 @@ __DEVICE__ inline f32 hgb_to_radians(f32 degrees) {
     return degrees * HGB_TAU / 360.0f;
 }
 
-__DEVICE__ inline f32 hgb_spow_preserve(f32 x, f32 p) {
+// Just set `HGB_SPOW`.
+enum Spow_Settings {
+    Spow_Preserve,
+    Spow_Clamp,
+    Spow_Mirror,
+};
+
+__DEVICE__ inline f32 hgb_spow(f32 x, f32 p) {
+    #if HGB_SPOW == Spow_Preserve
     if (x < 0.0f) {
         return x;
     }
     return hgb_pow(x, p);
-}
-
-__DEVICE__ inline f32 hgb_spow_mirror(f32 x, f32 p) {
+    #elif HGB_SPOW == Spow_Clamp
+    if (x < 0.0f) {
+        return 0.0f;
+    }
+    return hgb_pow(x, p);
+    #else
     return hgb_sign(x) * hgb_pow(hgb_abs(x), p);
+    #endif
 }
-
-#define HGB_ERROR make_float3(1.0f, 0.0f, 0.0f)
-#define HGB_WARNING make_float3(1.0f, 1.0f, 0.0f)
 
 __DEVICE__ inline f32 hgb_lerp(f32 a, f32 b, f32 t) {
     return (1.0f - t) * a + t * b;
 }
 
-__DEVICE__ inline f32 hgb_basis_gaussian(f32 x, f32 size) {
-    return hgb_exp(hgb_pow(x / size, 2.0f));
+__DEVICE__ inline f32 smoothstep(f32 a, f32 b, f32 x) {
+   f32 t = (x - a) / (b - a);
+   return hgb_square(t) * (3.0f - 2.0f * t);
 }
 
-__DEVICE__ inline f32 hgb_basis_tent(f32 x, f32 size) {
-    return hgb_max(1.0f - hgb_abs(x / size), 0.0f);
+__DEVICE__ inline f32 smootherstep(f32 a, f32 b, f32 x) {
+   f32 t = (x - a) / (b - a);
+   return hgb_cube(t) * (t * (6.0f * t - 15.0f) + 10.0f);
 }
 
-__DEVICE__ inline f32 hgb_basis_exponential(f32 x, f32 size) {
-    return hgb_exp(-hgb_abs(x / size));
-}
-
-__DEVICE__ inline f32 hgb_basis_falloff(f32 x, f32 size, f32 knee = 1.0f) {
-    if (x == 0.0f) {
-        return 1.0f;
-    }
-    return hgb_pow(1.0f / hgb_pow(x / size, 2.0f), knee);
-}
-
-__DEVICE__ inline f32 hgb_basis_boxcar(f32 x, f32 size) {
-    if (x < -size || x > size) {
+__DEVICE__ inline f32 step(f32 edge, f32 x) {
+    if (x < edge) {
         return 0.0f;
     }
     return 1.0f;
 }
 
+__DEVICE__ Vec2 hgb_vec2(f32 x, f32 y) {
+    Vec2 out = {x, y};
+    return out;
+}
+__DEVICE__ Vec3 hgb_vec3(f32 x, f32 y, f32 z) {
+    Vec3 out = {x, y, z};
+    return out;
+}
+__DEVICE__ Vec4 hgb_vec4(f32 x, f32 y, f32 z, f32 w) {
+    Vec4 out = {x, y, z, w};
+    return out;
+}
+
+__DEVICE__ Vec2 hgb_vec2_repeat(f32 v) { return hgb_vec2(v, v); }
+__DEVICE__ Vec3 hgb_vec3_repeat(f32 v) { return hgb_vec3(v, v, v); }
+__DEVICE__ Vec4 hgb_vec4_repeat(f32 v) { return hgb_vec4(v, v, v, v); }
+
+__DEVICE__ Vec2 hgb_vec2_zeros() { return hgb_vec2_repeat(0.0f); }
+__DEVICE__ Vec3 hgb_vec3_zeros() { return hgb_vec3_repeat(0.0f); }
+__DEVICE__ Vec4 hgb_vec4_zeros() { return hgb_vec4_repeat(0.0f); }
+
+__DEVICE__ Vec3 hgb_mat3_mul_vec3(Mat3 m, Vec3 v) {
+    return make_float3(
+        v.x * m[0][0] + v.y * m[0][1] + v.z * m[0][2],
+        v.x * m[1][0] + v.y * m[1][1] + v.z * m[1][2],
+        v.x * m[2][0] + v.y * m[2][1] + v.z * m[2][2]
+    );
+}
+
 typedef struct Stack Stack;
 struct Stack {
     __PRIVATE__ byte *data; // ?
-    uint size;
-    uint offset;
+    usize size;
+    usize offset;
 };
 
-__DEVICE__ void hgb_stack_init(__PRIVATE__ Stack *s, __PRIVATE__ byte *backing, uint size) {
+__DEVICE__ void hgb_stack_init(__PRIVATE__ Stack *s, __PRIVATE__ byte *backing, usize size) {
     s->data = backing;
     s->size = size;
     s->offset = 0;
 }
 
-__DEVICE__ void *hgb_stack_alloc(__PRIVATE__ Stack *s, uint amount) {
+__DEVICE__ void *hgb_stack_alloc(__PRIVATE__ Stack *s, usize amount) {
     void *ptr = s->data + s->offset;
     s->offset += amount;
     return ptr;
@@ -272,24 +305,24 @@ __DEVICE__ void hgb_stack_free_all(__PRIVATE__ Stack *s) {
 }
 
 __DEVICE__ void hgb_stack_free(__PRIVATE__ Stack *s, __PRIVATE__ void *ptr) {
-    s->offset = uint(ptr) - uint(s->data);
+    s->offset = usize(ptr) - usize(s->data);
 }
 
 typedef struct Arena Arena;
 struct Arena {
     __PRIVATE__ byte *data; // TODO
-    uint size;
-    uint offset;
+    usize size;
+    usize offset;
 };
 
-__DEVICE__ void arena_init(__PRIVATE__ Arena *a, __PRIVATE__ byte *backing, uint size) {
+__DEVICE__ void arena_init(__PRIVATE__ Arena *a, __PRIVATE__ byte *backing, usize size) {
     a->data = backing;
     a->size = size;
     a->offset = 0;
 }
 
-__DEVICE__ void *arena_alloc(__PRIVATE__ Arena *a, uint amount) {
-    uint total = a->offset + amount;
+__DEVICE__ void *arena_alloc(__PRIVATE__ Arena *a, usize amount) {
+    usize total = a->offset + amount;
     if (total <= a->size) {
         void *ptr = a->data + a->offset;
         a->offset = total;
@@ -306,12 +339,12 @@ typedef struct Linspace Linspace;
 struct Linspace {
     f32 delta;
     f32 start;
-    uint steps;
-    uint current;
+    usize steps;
+    usize current;
     bool done;
 };
 
-__DEVICE__ Linspace linspace_create(f32 start, f32 end, uint steps) {
+__DEVICE__ Linspace linspace_create(f32 start, f32 end, usize steps) {
     Linspace it;
     it.delta = (end - start) / f32(steps - 1);
     it.start = start;
@@ -335,10 +368,54 @@ __DEVICE__ f32 *linspace_allocate(__PRIVATE__ Arena *arena, Linspace it) {
     if (array == nil) {
         return nil;
     }
-    uint i = 0;
+    usize i = 0;
     while (!it.done) {
         array[i] = linspace_next(&it);
         i += 1;
     }
     return array;
+}
+
+__DEVICE__ inline f32 hgb_basis_gaussian(f32 x, f32 size) {
+    return hgb_exp(hgb_pow(x / size, 2.0f));
+}
+
+__DEVICE__ inline f32 hgb_basis_tent(f32 x, f32 size) {
+    return hgb_max(1.0f - hgb_abs(x / size), 0.0f);
+}
+
+__DEVICE__ inline f32 hgb_basis_exponential(f32 x, f32 size) {
+    return hgb_exp(-hgb_abs(x / size));
+}
+
+// Default to knee = 1.0f.
+__DEVICE__ inline f32 hgb_basis_falloff(f32 x, f32 size, f32 knee) {
+    if (x == 0.0f) {
+        return 1.0f;
+    }
+    return hgb_pow(1.0f / hgb_pow(x / size, 2.0f), knee);
+}
+
+__DEVICE__ inline f32 hgb_basis_boxcar(f32 x, f32 size) {
+    if (x < -size || x > size) {
+        return 0.0f;
+    }
+    return 1.0f;
+}
+
+// No function pointers in OpenCL.
+#define hgb_apply_tf(f, v) make_float3(f(v.x), f(v.y), f(v.z))
+
+__DEVICE__ float hgb_logc_encode(float x) {
+    if (x > 0.010591f) {
+        return 0.247190f * _log10f(5.555556f * x + 0.052272f) + 0.385537f;
+    }
+    return 5.367655f * x + 0.092809f;
+}
+
+__DEVICE__ float hgb_logc_decode(float x) {
+    if (x > 0.1496582f){
+        return (_powf(10.0f, (x - 0.385537f) / 0.2471896f) - 0.052272f) / 5.555556f;
+    }
+    return (x - 0.092809f) / 5.367655f;
 }
